e5eadef12eadbcfed3af51a71abc7b64657a7006
Jim Sullivan <jims@psws.com>
2017-09-11 13:32:08 -0700

    working tabs

--- uiroute.app/app/app.config.js
+++ uiroute.app/app/app.config.js
@@ -16,35 +16,75 @@
                 // State managing   
                 .state('home', {
                     url: '/home',
-                    templateUrl: '/app/test/index.html'
+                    views: {
+                        'contenthome@': {
+                            templateUrl: '/app/test/index.html'
+                        }
+                    }
                 })
                 // nested list with data  
                 .state('home.template', {
                     url: '/template',
-                    template: 'Welcome to the C# Corner'
-                })
+                    views: {
+                        'content': {
+                            template: 'Welcome to the C# Corner'
+                        }
+                    }
+               })
 
                 // nested list with controller
                 .state('home.list', {
                     url: '/list',
-                    templateUrl: '/app/test/homelist.html',
-                    controller: function ($scope) {
-                        $scope.Language = ['C#', 'VB', 'JavaScript', 'PHP'];
-                    }
+                        views: {
+                            'content': {
+                                templateUrl: '/app/test/homelist.html',
+                                controller: function ($scope) {
+                                    $scope.Language = ['C#', 'VB', 'JavaScript', 'PHP'];
+                                }
+                            }
+                        }
                 })
 
                 .state('home.grid', {
                     url: '/grid',
-                    templateUrl: '/app/test/grid.html',
-                    controller: 'gridController',
-                    controllerAs: 'vm'
+                    views: {
+                        'content': {
+                            templateUrl: '/app/test/grid.html',
+                            controller: 'gridController',
+                            controllerAs: 'vm'
+                        }
+                    }
+               })
+
+                .state('home.tab', {
+                    url: '/tab',
+                     views: {
+                        'content': {
+                            templateUrl: '/app/test/tab/tab.html',
+                            controller: 'tabController',
+                            controllerAs: 'vm'
+                        }
+                    }
+               })
+
+                .state('home.tab.grid', {
+                    url: '/grid',
+                    views: {
+                        'grid': {
+                            templateUrl: '/app/test/grid.html',
+                            controller: 'gridController as vm'
+                        }
+                    }
                 })
+
+
+
 
                 // State with multiple views  
                 .state('multipleview', {
                     url: '/multipleview',
                     views: {
-                        '': { templateUrl: '/app/test/multipleview.html' },
+                        'contenthome@': { templateUrl: '/app/test/multipleview.html' },
                         'ViewOne@multipleview': { template: 'Welcome to the C# Corner!' },
                         'ViewTwo@multipleview': {
                             templateUrl: '/app/test/dataList.html',
---
+++ uiroute.app/app/test/tab/tabcontroller.js
@@ -0,0 +1,78 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module("uiroute")
+        .controller('tabController', tabController)
+        ;
+
+    tabController.$inject = [
+        '$location',
+        '$state',
+        'tabService'
+
+    ];
+
+
+
+    function tabController($location, $state, tabService) {
+        //console.log("indexController init");
+
+        var vm = this;
+        vm.title = "Tabs";
+        vm.tabs = [];
+        vm.Selectedtab = "1"; //sets initial tab
+
+        vm.tabs = tabService.GetAllTabs('/app/test/tab/tab.tabs.json').then(function (data) {
+            vm.tabs = data;
+            console.log('tabController GetAllTabs() success...');
+        }).catch(function (response) {
+            console.log('GetAllTabs error...');
+            //deferred.reject("Tab file Failed to load");
+        });
+
+
+        vm.scrlTabsApi = {};
+        vm.tab1Show = false;
+
+        vm.tabSelected = function (route) {
+            $state.go(route);
+        };
+
+
+
+
+
+        //  http://plnkr.co/edit/YJNDaQ?p=preview
+        //  https://github.com/VersifitTechnologies/angular-ui-tab-scroll
+        vm.reCalcScroll = function () {
+            if (vm.scrlTabsApi.doRecalculate) {
+                vm.scrlTabsApi.doRecalculate();
+            }
+        };
+
+        vm.scrollIntoView = function (arg) {
+            if (vm.scrlTabsApi.scrollTabIntoView) {
+                vm.scrlTabsApi.scrollTabIntoView(arg);
+            }
+        };
+
+        vm.addTab = function () {
+            vm.tabs.push({
+                heading: 'New Tab ' + vm.tabs.length,
+                content: 'This is the content for a NEW tab ' + vm.tabs.length
+            });
+        };
+
+        vm.removeTab = function () {
+            vm.tabs.splice(vm.tabs.length - 1, 1);
+        };
+
+
+    }
+
+})();
+
+
+
+
